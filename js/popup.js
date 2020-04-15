cbcb0ddbe1fbfd3768c980a1e4bd02503cc9dd3e
Maria Borisova <maria@2-faces.ru>
2020-04-15 13:02:41 +0300

    + JS popup

--- js/popup.js
+++ js/popup.js
@@ -1,29 +1,54 @@
 let link = document.querySelector(".user-nav-link");
 let popup = document.querySelector(".modal-login");
 let close = document.querySelector(".modal-close");
-
 let login = popup.querySelector("[name=user-login]");
 let password = popup.querySelector("[name=user-password]");
-
 let form = popup.querySelector("form");
+let isStorageSupport = true;
+let storage = "";
+
+try{
+    storage=localStorage.getItem("login");
+} catch (err) {
+    isStorageSupport = false;
+}
+
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-    login.focus();
+
+    if (storage) {
+        login.value = storage;
+        password.focus();
+    } else {
+        login.focus();
+    }
 });
 
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-    console.log(login.value);
-    console.log(password.value);
-
-    if (!login.value || !password.value){
+    if (!login.value || !password.value) {
         console.log("Please enter login and password");
+        evt.preventDefault();
+        popup.classList.add("modal-error");
+    } else {
+        localStorage.setItem("login", login.value);
+    }
+});
+
+// closing modal block by escape
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error");
+        }
     }
 });
